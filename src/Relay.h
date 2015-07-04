916cdd9525bbb71d59489da2112576dd2b3e0e1c
Sebastian Kraus <sekdiy@gmail.com>
2015-07-04 23:07:43 +0200

    improved source code documentation, some minor corrections/improvements

--- src/Relay.h
+++ src/Relay.h
@@ -1,9 +1,11 @@
 /**
  * Relay library
  *
- * @author sekdiy
- * @date 18.06.2015
- * @version 1
+ * A no-frills, easy to use Arduino relay library for shields or modules.
+ *
+ * @author sekdiy (https://github.com/sekdiy/Relay)
+ * @date 4.07.2015
+ * @version See git comments for changes.
  */
 
 #ifndef RELAY_H
@@ -16,28 +18,31 @@
 #include "WProgram.h"
 #endif
 
-/**
- * Relay
- *
- * @version 1
- */
 class Relay
 {
   public:
-    Relay(int pin = LED_BUILTIN, int state = LOW, int mode = LOW); // Constructor
+    static const unsigned int ON = 1;      //!< Relay state: on.
+    static const unsigned int OFF = 0;     //!< Relay state: off.
 
-    void on();                    // Turn relay on
-    void off();                   // Turn relay off
-    void toggle();                // Toggle relay state
-    int getState();               // Get relay state (LOW/HIGH)
-    boolean isOn();               // Returns TRUE iff the relay is on
-    boolean isOff();              // Returns TRUE iff the relay is off
+    Relay(unsigned int pin = LED_BUILTIN,  //!< Arduino pin that the Relay is connected to (default: pin 13, usually the built-in LED)
+          unsigned int state = OFF,        //!< Initial state (default: off).
+          unsigned int mode = HIGH         //!< Logic mode (default: high-active, i.e. high level means on).
+         );                                //!< Initializes instance attributes, making sure the relay is in a sensible default state.
+
+    void on();                             //!< Turns the relay on.
+    void off();                            //!< Turns the relay off.
+    void toggle();                         //!< Toggles the relay state.
+    boolean isOn();                        //!< @return True iff the relay is on.
+    boolean isOff();                       //!< @return True iff the relay is off.
+
+    unsigned int getState();               //!< @return The current relay state (Relay::OFF or Relay::ON).
+    void setState(unsigned int state       //!< The new state to switch to (Relay::OFF or Relay::ON).
+                 );                        //!< Sets a relay state.
 
   protected:
-    int _mode;                    // control mode (noninverted/inverted)
-    int _state;                   // state model
-    int _pin;                     // pin model
-    void setState(int state);     // Set relay state (LOW/HIGH)
+    unsigned int _mode;                    //!< control mode (noninverted/inverted)
+    unsigned int _state;                   //!< state model
+    unsigned int _pin;                     //!< pin model
 };
 
 #endif   // RELAY_H
